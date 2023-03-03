a59fcc0641fabbadd933b5dfb73da49cd2725e49
devanshuchaudhary <devanshu88@gmail.com>
2023-03-03 14:51:01 +0530

    Initial Commit

---
+++ ChatGPTSwiftUI/ChatGPTSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTSwiftUIApp.swift
+//  ChatGPTSwiftUI
+//
+//  Created by devdchaudhary on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
